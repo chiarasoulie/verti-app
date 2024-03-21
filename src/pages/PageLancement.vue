09edf0f92e61f195b13aeedf2cf581f8a452b93a
Chiara <souliechiara@gmail.com>
2024-03-21 10:21:55 +0100

    ca avance

--- src/pages/PageLancement.vue
+++ src/pages/PageLancement.vue
@@ -6,43 +6,46 @@ import VertigoAppBar from '@/components/VertigoAppBar.vue';
    justify="center">
    <v-col>
     <VertigoAppBar></VertigoAppBar>
-  <v-app>
-    <v-main>
-      <v-container>
-        <!-- Première ligne -->
-        <v-row>
-          <v-col cols="12" md="6">
-            <v-img src="../assets/Stétoscope(1).png" max-height="90"></v-img>
-          </v-col>
-          <v-col cols="12" md="6">
-            <p>Texte pour la première ligne</p>
-          </v-col>
-        </v-row>
-        
-        <!-- Deuxième ligne -->
-        <v-row>
-          <v-col cols="12" md="6">
-            <v-img src="../assets/Oreille.png" max-height="90"></v-img>
-          </v-col>
-          <v-col cols="12" md="6">
-            <p>Texte pour la deuxième ligne</p>
-          </v-col>
-        </v-row>
-        
-        <!-- Troisième ligne -->
-        <v-row>
-          <v-col cols="12" md="6">
-            <v-img src="../assets/Group 8.png" max-height="90"></v-img>
-          </v-col>
-          <v-col cols="12" md="6">
-            <p>Texte pour la troisième ligne</p>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-main>
-  </v-app>
-  
-
+    <div class="page">
+      <div class="content">
+      <p class="image1">
+        <img src="../assets/Stétoscope(1).png" alt="image 1"/>
+      </p>
+      <p class="texte1">
+        Vous aurez besoin d'un interrogatoire détaillé et d'un examen clinique complet contenant :
+        <ul>
+        <li>Examen neurologique</li>
+        <li>Examen ORL</li>
+        <li>Dix et Hallpike</li>
+        <li>HINTS (Head Impulse, Nystagmus, Test of Skew)</li>
+      </ul>
+      </p>
+      <br>
+      <p class="image1">
+        <img src="../assets/Oreille.png" alt="image 1"/>
+      </p>
+      <p class="texte1">
+        Faux vertiges éliminés? <br>
+(Lipothymie, manifestations cardio-vasculaires, flou visuel, crise d'épilepsie, manifestations phobiques, fatigue...)
+      </p>
+      <br>
+      <p class="image1">
+        <img src="../assets/Group 8.png" alt="image 1"/>
+      </p>
+      <p class="texte1">
+        Un doute ? <br>
+Pour plus d'infos, rendez-vous sur le site (VERTI'WEB) !
+      </p>
+      <br><br>
+      <v-btn class="mr-4"
+            size="x-large"
+            rounded="xl"
+            variant="elevated"
+            to="/PageDrapeauxRouges">
+            Lancer le diagnostic
+      </v-btn>
+    </div>
+    </div>
    </v-col>
  </v-row>
 </template>
@@ -50,3 +53,32 @@ import VertigoAppBar from '@/components/VertigoAppBar.vue';
 <script setup>
 import VertigoAppBar from '@/components/VertigoAppBar.vue';
 </script>
+
+<style scoped>
+.mr-4{
+    background-color: #20285F; 
+    color: white; 
+}
+.image1{
+  grid-column: 1;
+  margin-right: auto;
+  margin-right: auto;
+  float: left;
+  width: 20%;
+}
+.texte1 {
+  grid-column: 2;
+  text-align: left;
+  overflow: hidden;
+  flex: 1;
+  width: 80%;
+  color:#20285F;
+  font-family: Manjari;
+}
+.page {
+  display: grid;
+  grid-template-columns: auto auto;
+  align-items: center;
+  width: 100%;
+}
+</style>
--- src/pages/PageVertigesMaintenant.vue
+++ src/pages/PageVertigesMaintenant.vue
@@ -3,26 +3,25 @@
       align="center" 
       justify="center">
       <v-col>
-      <p>Y-a-t-il des vertiges maintenant ?</p>
+        <VertigoAppBar></VertigoAppBar>
+      <h2 class="question">Y-a-t-il des vertiges maintenant ?</h2>
       <p>{{ vertigesMaintenant }}</p>
 
       <v-btn-toggle v-model="vertigesMaintenant" color="deep-purple-accent-3">
-        <v-btn 
-              size="x-large"
-              rounded="xl"
-              variant="elevated"
-              value="oui"
-              to="/PageHints">
-              Oui
-          </v-btn>
-          <v-btn 
-              size="x-large"
-              rounded="xl"
-              variant="elevated"
-              value="non"
-              to="/PageNystagmusSpontane">
-              Non
-          </v-btn>
+        <v-btn class="mr-4"
+            size="x-large"
+            rounded="xl"
+            variant="elevated"
+            to="/PageHints">
+            Non
+      </v-btn>
+      <v-btn class="bouton"
+            size="x-large"
+            rounded="xl"
+            variant="elevated"
+            to="/PageNystagmusSpontane">
+            Oui
+      </v-btn>
       </v-btn-toggle>
       </v-col>
     </v-row>
@@ -32,6 +31,7 @@
     
 <script setup>
   import VertigoBackButton from '@/components/VertigoBackButton.vue';
+  import VertigoAppBar from '@/components/VertigoAppBar.vue';
 
   // -- utilisation du state global
   import { storeToRefs } from 'pinia'
@@ -39,4 +39,18 @@
   const app = useAppStore()
   const { vertigesMaintenant } = storeToRefs(app)
 </script>
+
+<style scoped>
+.bouton{
+    background-color: #20285F; 
+    color: white; 
+}
+.mr-4{
+    border: 2px solid #20285F;
+}
+.question{
+    color:#20285F;
+    font-family: Manjari;
+}
+</style>
     
